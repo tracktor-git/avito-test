ebc308a2c18eed9b7f0867fcc0f35e1636e9312c
Tracktor <tracktor@bk.ru>
2024-09-12 07:05:36 +0300

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import Advertisements from './Components/Advertisements';
 import Advertisement from './Components/Advertisement';
 import Orders from './Components/Orders';
 import Navigation from './Components/Navigation';
+import Footer from './Components/Footer';
 
 const App = () => (
   <Router>
     <Navigation />
-    <Routes>
-      <Route path="/" element={<Advertisements />} />
-      <Route path="/advertisements/:id" element={<Advertisement />} />
-      <Route path="/orders" element={<Orders />} />
-    </Routes>
+    <main>
+      <Routes>
+        <Route path="/" element={<Advertisements />} />
+        <Route path="/advertisements/:id" element={<Advertisement />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </main>
+    <Footer />
   </Router>
 );
 
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <p className="copyright">Tracktor © 2024</p>
+      <a className="github" href="https://github.com/tracktor-git" target="_blank" rel="noreferrer" title="GitHub">
+        <i className="pi pi-github" />
+      </a>
+    </div>
+  </footer>
+);
+
+export default Footer;
